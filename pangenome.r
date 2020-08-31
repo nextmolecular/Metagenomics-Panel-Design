2145de3dc6b8a271dffc267707958af03d4a973f
nextmolecular <greg.riddick@nextmolecular.com>
2020-08-31 15:26:51 -0400

    get.orf.clusters

--- pangenome.r
+++ pangenome.r
@@ -139,15 +139,92 @@ return(bgv)
 
 
 
+ 
+
+get.core.genome = function(orf.fasta,species.fasta,orf.core.fasta){
+
+#Purpose:Compute the core set of ORFs for a given set of strains in a species
+#Input:
+#  orf.fasta: name of the fasta file holding the species ORFs 
+#  species.fasta: name of the fasta file holding the species genomes
+#  orf.core.fasta: name for the fasta file to be output for the core species ORFs
+#Output:
+#  orf.core.fasta: fasta file of the core species ORFs
+#  orf.genome.blast (returned): tibble holding the result of blasting ORFs against species genomes
+
+#command line for making species blastdb
+print("Creating Species Database ...")
+system(paste("makeblastdb -in ",species.fasta," -dbtype nucl"))
 
 
-get.core.genome = function(fastafile,species.database){
+#command line for making orf blastdb
+print("Creating ORF Database ...")
+system(paste("makeblastdb -in ",orf.fasta," -dbtype nucl"))
 
 
+#command line for blasting
+print("Blasting ORF vs Species Database ...")
+system(paste("megablast -d ",species.fasta," -i ",orf.fasta," -D 3 > e1.txt")) 
+
+#parse the megablast result file
+#and find core orfs shared by all strains
+orf.genome.blast = read_tsv("e1.txt",comment="#",col_names=F)
+
+
+	   
+e2 = orf.genome.blast %>%
+     group_by(X1) %>%
+	 summarize(target.genomes = n_distinct(X2)) %>%
+	 filter(target.genomes ==9) %>%
+	 mutate(orf = as.numeric(str_split_fixed(X1,":",n=3)[,3])) %>%
+	 mutate(source.organism.a  = str_split_fixed(X1,":",n=3)[,1]) %>%
+	 mutate(source.organism.b  = str_split_fixed(X1,":",n=3)[,2]) %>%
+	 mutate(source.organism = str_c(source.organism.a,source.organism.b,sep= " ")) %>%
+	 arrange(source.organism,orf) %>%
+	 select(source.organism,orf,X1) 
+	 
+	 
+fres =readDNAStringSet(orf.fasta)	 
+f2 = fres[names(fres) %in% e2$X1]
+writeXStringSet(f2,orf.core.fasta)
+
+
+return(orf.genome.blast)
 
 
 
 }
+
+
+
+
+
+
+
+get.orf.clusters = function(orf.genome.blast,centers=2){
+#Purpose:
+#Input:
+#Output:
+
+#find cluster of strains
+e3 = orf.genome.blast %>%
+     select(X1,X2) %>%
+	 distinct(.) %>%
+	 mutate(strain = as.vector(str_split_fixed(X1,":[:digit:]+$",n=2)[,1])) %>%
+	 select(source.strain = X2, target.strain = strain) %>%
+	 group_by(source.strain,target.strain) %>%
+	 summarize(n()) %>%
+	 select(source.strain=source.strain,target.strain = target.strain,count='n()') %>%
+     spread(target.strain,count) %>%
+     column_to_rownames("source.strain")	 
+	  
+e3.kmeans  = kmeans(e3,centers=centers)
+
+return(e3.kmeans$cluster)	  
+
+}	  
+	  
+
 
 
 
@@ -254,63 +331,6 @@ for (i in 2:length(bg)){
 writeXStringSet(fres,"ehrlicia.1.orf.fasta")
 
 
- 
-
-get.core.genome = function(orf.fasta,species.fasta,orf.core.fasta){
-
-#Purpose:Compute the core set of ORFs for a given set of strains in a species
-#Input:
-#  orf.fasta: name of the fasta file holding the species ORFs 
-#  species.fasta: name of the fasta file holding the species genomes
-#  orf.core.fasta: name for the fasta file to be output for the core species ORFs
-#Output:
-#  orf.core.fasta: fasta file of the core species ORFs
-#  orf.genome.blast (returned): tibble holding the result of blasting ORFs against species genomes
-
-#command line for making species blastdb
-print("Creating Species Database ...")
-system(paste("makeblastdb -in ",species.fasta," -dbtype nucl"))
-
-
-#command line for making orf blastdb
-print("Creating ORF Database ...")
-system(paste("makeblastdb -in ",orf.fasta," -dbtype nucl"))
-
-
-#command line for blasting
-print("Blasting ORF vs Species Database ...")
-system(paste("megablast -d ",species.fasta," -i ",orf.fasta," -D 3 > e1.txt")) 
-
-#parse the megablast result file
-#and find core orfs shared by all strains
-orf.genome.blast = read_tsv("e1.txt",comment="#",col_names=F)
-
-
-	   
-e2 = orf.genome.blast %>%
-     group_by(X1) %>%
-	 summarize(target.genomes = n_distinct(X2)) %>%
-	 filter(target.genomes ==9) %>%
-	 mutate(orf = as.numeric(str_split_fixed(X1,":",n=3)[,3])) %>%
-	 mutate(source.organism.a  = str_split_fixed(X1,":",n=3)[,1]) %>%
-	 mutate(source.organism.b  = str_split_fixed(X1,":",n=3)[,2]) %>%
-	 mutate(source.organism = str_c(source.organism.a,source.organism.b,sep= " ")) %>%
-	 arrange(source.organism,orf) %>%
-	 select(source.organism,orf,X1) 
-	 
-	 
-fres =readDNAStringSet(orf.fasta)	 
-f2 = fres[names(fres) %in% e2$X1]
-writeXStringSet(f2,orf.core.fasta)
-
-
-return(orf.genome.blast)
-
-
-
-}
-
-
 
 res = get.core.genome("ehrlicia.1.orf.fasta","Ehr.fasta","ehrlicia.1.core.orf.fasta")
 
@@ -319,21 +339,9 @@ res = get.core.genome("ehrlicia.1.orf.fasta","Ehr.fasta","ehrlicia.1.core.orf.fa
 
 
 
-#find cluster of strains
-e3 = e1 %>%
-     select(X1,X2) %>%
-	 distinct(.) %>%
-	 mutate(strain = as.vector(str_split_fixed(X1,":[:digit:]+$",n=2)[,1])) %>%
-	 select(source.strain = X2, target.strain = strain) %>%
-	 group_by(source.strain,target.strain) %>%
-	 summarize(n()) %>%
-	 select(source.strain=source.strain,target.strain = target.strain,count='n()') %>%
-     spread(target.strain,count) %>%
-     column_to_rownames("source.strain")	 
-	  
-	  
-	  
-	  
+
+
+
 
 ##############################################################
 #  Find common kmers  ########################################
